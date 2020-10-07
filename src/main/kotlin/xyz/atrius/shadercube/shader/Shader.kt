ec433a014b12cbe49d321de5070f2b3b20cf536e
Atrius <atriusedm@gmail.com>
2020-10-07 16:18:23 -0400

    Add rotational methods
    This will need to be cleaned up later

--- src/main/kotlin/xyz/atrius/shadercube/shader/Shader.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shader/Shader.kt
@@ -1,9 +1,11 @@
 package xyz.atrius.shadercube.shader
 
-import org.bukkit.Location
-import org.bukkit.Rotation
+import com.destroystokyo.paper.ParticleBuilder
+import org.bukkit.*
 import org.bukkit.util.Vector
-import java.awt.Dimension
+import xyz.atrius.shadercube.KotlinPlugin
+import kotlin.math.cos
+import kotlin.math.sin
 
 interface Shader {
 
@@ -21,13 +23,15 @@ interface Shader {
     var setup: () -> Unit
 
     var update: () -> Unit
-}
 
-infix fun Int.by(other: Int) =
-    Vector(this, 0, other)
+    fun Vector.rotateX(angle: Double, offset: Double = 0.0, center: Vector = position.toVector()) =
+        subtract(center).add(Vector(0.0, offset, offset)).rotateAroundX(angle).add(center)
 
-infix fun Vector.by(y: Int) = also {
-    this.y = y.toDouble()
+    fun Vector.rotateY(angle: Double, offset: Double = 0.0, center: Vector = position.toVector()) =
+        subtract(center).add(Vector(offset, 0.0, offset)).rotateAroundY(angle).add(center)
+
+    fun Vector.rotateZ(angle: Double, offset: Double = 0.0, center: Vector = position.toVector()) =
+        subtract(center).add(Vector(offset, offset, 0.0)).rotateAroundZ(angle).add(center)
 }
 
 operator fun Vector.component1() = blockX
@@ -36,17 +40,21 @@ operator fun Vector.component2() = blockY
 
 operator fun Vector.component3() = blockZ
 
-fun shader(shader: Shader.() -> Unit): Shader {
+fun shader(plugin: KotlinPlugin, shader: Shader.() -> Unit): Shader {
     val shade = object : Shader {
         override var position: Location =
             Location(null, 0.0, 0.0, 0.0)
-        override var rotation: Vector = 0 by 0 by 0
-        override var size: Vector = 5 by 5
+        override var rotation: Vector = Vector(0.0, 0.0, 0.0)
+        override var size: Vector = Vector(5.0, 0.0, 5.0)
         override var centered: Boolean = false
         override var setup  = {}
         override var update = {}
     }
     shader(shade)
+    shade.setup()
+    Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, {
+        shade.update()
+    }, 0L, 1L)
     return shade
 }
 
@@ -56,4 +64,16 @@ fun Shader.setup(block: () -> Unit) {
 
 fun Shader.update(block: () -> Unit) {
     update = block
+}
+
+fun Shader.particle(particle: Particle, block: ParticleBuilder.() -> Unit) {
+    val builder = ParticleBuilder(particle).location(position)
+    block(builder)
+    builder.spawn()
+}
+
+interface Shape {
+    val center: Vector
+
+    val points: Array<Vector>
 }
