7b9f19b2904d586dcb17945c47663f2dcbdc0933
Atrius <atriusedm@gmail.com>
2020-10-11 04:13:57 -0400

    Implement n-sided polygon shapes

---
+++ src/main/kotlin/xyz/atrius/shadercube/shape/Polygon.kt
@@ -0,0 +1,33 @@
+package xyz.atrius.shadercube.shape
+
+import org.bukkit.Location
+import org.bukkit.Particle
+import org.bukkit.util.Vector
+import xyz.atrius.shadercube.util.radians
+import xyz.atrius.shadercube.util.vec2d
+
+class Polygon(
+        override var point   : Location,
+        override var particle: Particle,
+                     size    : Double = 1.0,
+                     faces   : Int    = 3,
+                     vertexes: Int    = 25,
+        override val block   : Style<Polygon>
+) : Shape<Polygon> {
+    override val size: Vector = size.vec2d
+    override val points: Array<Vector> = Array(faces) { point.toVector() }
+
+    init {
+        val angle = (360.0 / faces).radians
+        points.forEachIndexed { i, p ->
+            p.rotateY(angle * i, size)
+        }
+        points.forEachIndexed { i, p ->
+            Line(
+                p.toLocation(world), points[(i + 1) % faces].toLocation(world), particle, vertexes
+            ) { _, v ->
+                block(this@Polygon, v)
+            }
+        }
+    }
+}
