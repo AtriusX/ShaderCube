c9fc5022dc2697a154df51304799c57d5e99699e
Atrius <atriusedm@gmail.com>
2020-10-11 04:14:12 -0400

    Create new polygon shader

--- src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
+++ src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
@@ -1,6 +1,7 @@
 package xyz.atrius.shadercube
 
 import org.bukkit.Color
+import org.bukkit.Particle
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -11,8 +12,8 @@ import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import xyz.atrius.shadercube.shader.Shader
 import xyz.atrius.shadercube.shader.shader
-import xyz.atrius.shadercube.shape.Cube
-import xyz.atrius.shadercube.util.vec
+import xyz.atrius.shadercube.shape.Polygon
+import xyz.atrius.shadercube.util.radians
 
 typealias KotlinPlugin =
     JavaPlugin
@@ -35,21 +36,20 @@ class ShaderCube : KotlinPlugin(), Listener {
         players[event.player] = shader {
             update {
                 point = event.player.location.add(0.0, 1.0, 0.0)
-                val angle = time / 7500.0
-                Cube(point, size = 3.vec, step = 3) { _, v ->
-                    location(v.rotateX(angle * 2).rotateY(angle).rotateZ(-angle * 3))
+                Polygon(point, Particle.REDSTONE, 3.0) { p, v ->
+                    location(v.rotateY(time / 1500.0))
                     color(Color.YELLOW, 0.5f)
                 }
-                Cube(point, size = 3.vec, step = 3) { _, v ->
-                    location(v.rotateX(angle * 3).rotateY(-angle * 2).rotateZ(angle))
+                Polygon(point, Particle.REDSTONE, 3.0) { _, v ->
+                    location(v.rotateY(40.radians + time / 1500.0))
                     color(Color.AQUA, 0.5f)
                 }
-                Cube(point, size = 3.vec, step = 3) { _, v ->
-                    location(v.rotateX(-angle * 2).rotateY(angle).rotateZ(-angle * 1.5))
+                Polygon(point, Particle.REDSTONE, 3.0) { _, v ->
+                    location(v.rotateY(80.radians + time / 1500.0))
                     color(Color.RED, 0.5f)
                 }
-                Cube(point, size = 3.vec, step = 3) { _, v ->
-                    location(v.rotateX(-angle * 3).rotateY(-angle * 1.5).rotateZ(angle))
+                Polygon(point, Particle.REDSTONE, 1.0, 6) { _, v ->
+                    location(v.rotateY(time / 1500.0))
                     color(Color.LIME, 0.5f)
                 }
             }
