1dbc03c259247da2cd2fcfa8692bf36a543ebd03
Atrius <atriusedm@gmail.com>
2020-10-12 04:11:21 -0400

    More complex shader example

--- src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
+++ src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
@@ -1,15 +1,19 @@
 package xyz.atrius.shadercube
 
+import com.destroystokyo.paper.ParticleBuilder
 import org.bukkit.Color
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.util.Vector
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import xyz.atrius.shadercube.shader.shader
 import xyz.atrius.shadercube.shape.Circle
 import xyz.atrius.shadercube.shape.Star
+import xyz.atrius.shadercube.util.radians
 import kotlin.math.abs
 import kotlin.math.sin
 
@@ -29,26 +33,31 @@ class ShaderCube : KotlinPlugin(), Listener {
 
     @EventHandler
     fun onJoin(event: PlayerMoveEvent) {
+        val player = event.player
+        if (!player.isGliding) {
+            return
+        }
         shader {
-            point = event.player.location.add(0.0, 1.0, 0.0)
-            Circle(point,
-                size     = 4.0,
-                vertexes = 100
-            ) { (v) ->
-                color(Color.RED)
-                location(v.rotateY(time / 2000.0).rotateX(time / 1500.0).rotateZ(time / 3000.0))
-            }
-            Star(point,
-                size   = 4.0,
-                points = 3 + abs(sin(time / 4000.0) * 9).toInt(),
-                jump   = 1 + abs(sin(time / 2500.0) * 3).toInt()
-            ) { (v) ->
+            fun ParticleBuilder.update(player: Player, v: Vector) {
                 color(Color.YELLOW)
-                location(v.rotateY(time / 2000.0).rotateX(time / 1500.0).rotateZ(time / 3000.0))
+                location(v.rotateX((90.0 + point.pitch).radians,
+                        center = player.eyeLocation.toVector())
+                        .rotateY(-point.yaw.toDouble().radians)
+                )
             }
+            point = player.location.add(0.0, 9.0, 0.0)
+            val size = 4.0
+            Circle(point,
+                size     = size,
+                vertexes = 100
+            ) { (v) -> update(player, v) }
+            Star(point,
+                size   = size,
+                points = 3 + abs(sin(time / 2000.0) * 9).toInt(),
+                jump   = 1 + abs(sin(time / 500.0) * 3).toInt()
+            ) { (v) -> update(player, v) }
         }
     }
-
 }
 
 
