f45cec86da93d3ebb69bce8f14169b07f5fe247f
Atrius <atriusedm@gmail.com>
2020-10-13 03:51:56 -0400

    Allow sounds to work in spatial objects

--- src/main/kotlin/xyz/atrius/shadercube/shader/SoundData.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shader/SoundData.kt
@@ -3,6 +3,7 @@ package xyz.atrius.shadercube.shader
 import org.bukkit.Sound
 import org.bukkit.SoundCategory
 import org.bukkit.util.Vector
+import xyz.atrius.shadercube.Spatial
 
 typealias SoundBuilder =
     SoundData.() -> Unit
@@ -14,7 +15,7 @@ data class SoundData(
     var pitch   : Double        = 1.0
 )
 
-fun Shader.sound(sound: Sound? = null, block: SoundBuilder) {
+fun Spatial.sound(sound: Sound? = null, block: SoundBuilder) {
     val (point, category, volume, pitch) = SoundData().apply(block)
     world.playSound(
         point?.toLocation(world) ?: this.point, sound ?: return,
