dd7cd9fb44f4a5e740e0367a36d98edfe6fb6375
Atrius <atriusedm@gmail.com>
2020-10-13 03:50:42 -0400

    Orbit should not introduce a shader scope

--- src/main/kotlin/xyz/atrius/shadercube/data/Orbit.kt
+++ src/main/kotlin/xyz/atrius/shadercube/data/Orbit.kt
@@ -1,21 +1,25 @@
 package xyz.atrius.shadercube.data
 
 import org.bukkit.Location
-import org.bukkit.util.Vector
+import xyz.atrius.shadercube.Spatial
 import xyz.atrius.shadercube.shader.Shader
+import xyz.atrius.shadercube.shape.Data
 import xyz.atrius.shadercube.util.radians
+
+typealias OrbitData =
+    Spatial.(Data<Orbit>) -> Unit
 
 class Orbit(
     override var point: Location,
-    private  val size : Double                  = 1.0,
-    private  val rate : Double                  = 1.0,
-    private  val block: Shader.(Vector) -> Unit = {}
-) : Shader() {
+             var size : Double    = 1.0,
+             var rate : Double    = 1.0,
+    private  val block: OrbitData = {}
+) : Spatial {
 
     var offset: Double = 0.0
 
-    operator fun invoke() = block(
-        point.clone().toVector().rotateY(((time / 10000) * rate + offset).radians, size)
+    fun update() = block(Data(point.clone().toVector()
+        .rotateY(((System.currentTimeMillis() / 10) * rate + offset).radians, size), this)
     )
 
     operator fun plusAssign(amt: Int) =
@@ -23,7 +27,7 @@ class Orbit(
 
     operator fun plusAssign(amt: Double) {
         offset += amt
-        this()
+        update()
     }
 
     operator fun inc() = also { this += 1 }
@@ -33,15 +37,15 @@ class Orbit(
 
     operator fun minusAssign(amt: Double) {
         offset -= amt
-        this()
+        update()
     }
 
     operator fun dec() = also { this -= 1 }
 }
 
 fun Shader.orbit(
-    point: Location = this.point,
-    size : Double   = 1.0,
-    rate : Double   = 1.0,
-    block: Shader.(Vector) -> Unit
+    point: Location  = this.point,
+    size : Double    = 1.0,
+    rate : Double    = 1.0,
+    block: OrbitData = {}
 ) = Orbit(point, size, rate, block)
