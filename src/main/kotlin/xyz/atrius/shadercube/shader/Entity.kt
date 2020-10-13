f01c6cf3b7d5e95099b0b764840ef3cdc949db85
Atrius <atriusedm@gmail.com>
2020-10-13 14:35:20 -0400

    Add lightning and falling block support

--- src/main/kotlin/xyz/atrius/shadercube/shader/Entity.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shader/Entity.kt
@@ -1,19 +1,36 @@
 package xyz.atrius.shadercube.shader
 
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.entity.Entity
+import org.bukkit.entity.FallingBlock
 import org.bukkit.util.Vector
 
 inline fun <reified T : Entity> Shader.entity(
     point         : Vector       = this.point.toVector(),
+    effect        : Boolean      = false,
     noinline block: T.() -> Unit = {}
 ) = try {
-    // I absolutely hate this but there doesn't appear to be a better way
-    if (T::class.simpleName == "LightningStrike") {
-        val l = world.strikeLightning(point.toLocation(world))
-    } else {
-        world.spawn(point.toLocation(world), T::class.java, block)
-    }
+    world.spawn(point.toLocation(world), T::class.java, block)
 } catch (e: Exception) {
     Bukkit.getLogger().warning(e.message)
+    null
+}
+
+fun Shader.lightning(
+    point : Vector  = this.point.toVector(),
+    effect: Boolean = false
+) = if (effect)
+    world.strikeLightningEffect(point.toLocation(world))
+else
+    world.strikeLightning(point.toLocation(world))
+
+fun Shader.fallingBlock(
+    material: Material             = Material.SAND,
+    point: Vector                  = this.point.toVector(),
+    block: FallingBlock.() -> Unit = {}
+) {
+    world.spawnFallingBlock(
+        point.toLocation(world), material.createBlockData()
+    ).apply(block)
 }
