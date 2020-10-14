1a84f5ea06db6aa17d0abdf5798302d319c78405
Atrius <atriusedm@gmail.com>
2020-10-13 23:40:01 -0400

    Implement potion API for DSL

---
+++ src/main/kotlin/xyz/atrius/shadercube/shader/Potion.kt
@@ -0,0 +1,21 @@
+package xyz.atrius.shadercube.shader
+
+import org.bukkit.entity.LivingEntity
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+
+typealias PotionBuilder =
+    PotionEffectData.() -> Unit
+
+data class PotionEffectData(
+    var duration : Int = 10000,
+    var amplifier: Int = 1,
+    var ambient  : Boolean = true,
+    var particles: Boolean = ambient,
+    var icon     : Boolean = ambient
+)
+
+fun LivingEntity.potion(effect: PotionEffectType, block: PotionBuilder = {}) {
+    val (duration, amplifier, ambient, particles, icon) = PotionEffectData().apply(block)
+    addPotionEffect(PotionEffect(effect, duration, amplifier, ambient, particles, icon))
+}
