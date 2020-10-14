3076eb13ae1cfcd73339a64c20c711b46e6aeb3c
Atrius <atriusedm@gmail.com>
2020-10-14 18:27:33 -0400

    Reimplement location methods

--- src/main/kotlin/xyz/atrius/shadercube/shader/Entity.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shader/Entity.kt
@@ -1,33 +1,48 @@
 package xyz.atrius.shadercube.shader
 
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.entity.Entity
 import org.bukkit.entity.FallingBlock
 import org.bukkit.util.Vector
 
 inline fun <reified T : Entity> Shader.entity(
-    point         : Vector       = this.point,
+    point         : Vector,
+    noinline block: T.() -> Unit = {}
+) = entity(point.toLocation(world), block)
+
+inline fun <reified T : Entity> Shader.entity(
+    location         : Location  = this.location,
     noinline block: T.() -> Unit = {}
 ) = try {
-    world.spawn(point.toLocation(world), T::class.java, block)
+    world.spawn(location, T::class.java, block)
 } catch (e: Exception) {
     Bukkit.getLogger().warning(e.message)
     null
 }
 
 fun Shader.lightning(
-    point : Vector  = this.point,
+    point : Vector,
     effect: Boolean = false
+) = lightning(point.toLocation(world), effect)
+
+fun Shader.lightning(
+    location: Location = this.location,
+    effect  : Boolean  = false
 ) = if (effect)
-    world.strikeLightningEffect(point.toLocation(world))
+    world.strikeLightningEffect(location)
 else
-    world.strikeLightning(point.toLocation(world))
+    world.strikeLightning(location)
 
 fun Shader.fallingBlock(
     material: Material                = Material.SAND,
-    point   : Vector                  = this.point,
+    point   : Vector,
     block   : FallingBlock.() -> Unit = {}
-) = world.spawnFallingBlock(
-    point.toLocation(world), material.createBlockData()
-).apply(block)
+) = fallingBlock(material, point.toLocation(world), block)
+
+fun Shader.fallingBlock(
+    material: Material                = Material.SAND,
+    location: Location                = this.location,
+    block   : FallingBlock.() -> Unit = {}
+) = world.spawnFallingBlock(location, material.createBlockData()).apply(block)
