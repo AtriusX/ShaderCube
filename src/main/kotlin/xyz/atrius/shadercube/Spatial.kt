0fa9163692f90dacb65a9c0001ebe8c34c0ce892
Atrius <atriusedm@gmail.com>
2020-10-14 18:43:38 -0400

    Implement location spatial methods

--- src/main/kotlin/xyz/atrius/shadercube/Spatial.kt
+++ src/main/kotlin/xyz/atrius/shadercube/Spatial.kt
@@ -19,18 +19,28 @@ interface Spatial {
     fun Vector.rotateX(angle: Double, offset: Double = 0.0, center: Vector = point) =
         subtract(center).add(Vector(0.0, offset, 0.0)).rotateAroundX(angle).add(center)
 
+    fun Vector.rotateX(angle: Double, offset: Double = 0.0, center: Location) =
+        rotateX(angle, offset, center.toVector())
+
     fun Vector.rotateY(angle: Double, offset: Double = 0.0, center: Vector = point) =
         subtract(center).add(Vector(offset, 0.0, 0.0)).rotateAroundY(angle).add(center)
+
+    fun Vector.rotateY(angle: Double, offset: Double = 0.0, center: Location) =
+        rotateY(angle, offset, center.toVector())
 
     fun Vector.rotateZ(angle: Double, offset: Double = 0.0, center: Vector = point) =
         subtract(center).add(Vector(0.0, offset, 0.0)).rotateAroundZ(angle).add(center)
 
-    fun particle(particle: Particle, position: Vector = point, block: ParticleBuilder.() -> Unit) {
-        val builder = ParticleBuilder(particle).location(position.toLocation(world))
-        block(builder)
-        builder.spawn()
-    }
+    fun Vector.rotateZ(angle: Double, offset: Double = 0.0, center: Location) =
+        rotateZ(angle, offset, center.toVector())
+
+    fun particle(particle: Particle, position: Location, block: ParticleBuilder.() -> Unit) =
+        ParticleBuilder(particle).location(position).also(block).spawn()
+
+    fun particle(particle: Particle, position: Vector = point, block: ParticleBuilder.() -> Unit) =
+        particle(particle, position.toLocation(world), block)
 
     fun ParticleBuilder.location(vector: Vector) =
         location(vector.toLocation(world))
 }
+
