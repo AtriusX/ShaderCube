4d7134a2a8fad8cdc9e3c8cd18011835cb280f20
Atrius <atriusedm@gmail.com>
2020-10-15 00:39:51 -0400

    Add rgb and hsb color functions

--- src/main/kotlin/xyz/atrius/shadercube/util/Misc.kt
+++ src/main/kotlin/xyz/atrius/shadercube/util/Misc.kt
@@ -1,7 +1,17 @@
 package xyz.atrius.shadercube.util
 
+import org.bukkit.Color
+import java.awt.Color.HSBtoRGB
+
 val Double.radians: Double
     get() = Math.toRadians(this)
 
 val Int.radians: Double
     get() = Math.toRadians(this.toDouble())
+
+fun rgb(r: Int, g: Int, b: Int) =
+    Color.fromRGB(r, g, b)
+
+fun hsb(hue: Float, saturation: Float, brightness: Float) = Color.fromRGB(
+    HSBtoRGB(hue, saturation, brightness) and 0xffffff
+)
