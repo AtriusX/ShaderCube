bac7704f434e198497523588236f7c0681ad4e8d
Atrius <atriusedm@gmail.com>
2020-10-17 02:18:16 -0400

    Add clamp functions

--- src/main/kotlin/xyz/atrius/shadercube/util/Math.kt
+++ src/main/kotlin/xyz/atrius/shadercube/util/Math.kt
@@ -1,5 +1,8 @@
 package xyz.atrius.shadercube.util
 
+import kotlin.math.max
+import kotlin.math.min
+
 val Double.radians: Double
     get() = Math.toRadians(this)
 
@@ -11,3 +14,9 @@ fun lerp(a: Double, b: Double, i: Double): Double =
 
 fun lerp(a: Int, b: Int, i: Double): Int =
     lerp(a.toDouble(), b.toDouble(), i).toInt()
+
+fun clamp(min: Double, max: Double, value: Double): Double =
+    min(max(min, value), max)
+
+fun clamp(min: Int, max: Int, value: Int): Int =
+    clamp(min.toDouble(), max.toDouble(), value.toDouble()).toInt()
