746ac0e6083709f36cfc1f057abc61df2c4f3f31
Atrius <atriusedm@gmail.com>
2020-10-17 02:18:44 -0400

    Test gradients and blend modes

--- src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
+++ src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
@@ -1,16 +1,19 @@
 package xyz.atrius.shadercube
 
 import org.bukkit.Color
+import org.bukkit.Location
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.util.Vector
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import xyz.atrius.shadercube.shader.shader
-import xyz.atrius.shadercube.shape.circle
-import xyz.atrius.shadercube.util.plusCompliments
-import xyz.atrius.shadercube.util.radians
+import xyz.atrius.shadercube.shape.line
+import xyz.atrius.shadercube.util.Blend
+import xyz.atrius.shadercube.util.blend
+import xyz.atrius.shadercube.util.plusGradients
 
 typealias KotlinPlugin =
     JavaPlugin
@@ -31,31 +34,18 @@ class ShaderCube : KotlinPlugin(), Listener {
     fun onJoin(event: PlayerJoinEvent) {
         val player = event.player
         shader {
-            val colors = Color.FUCHSIA.plusCompliments(10)
+            location = Location(player.world, 0.0, 90.0, 0.0)
+            val gradients = Color.RED.plusGradients(Color.LIME, Color.WHITE, points = 15)
+            val blends = arrayOf(Blend.ADD, Blend.SUBTRACT, Blend.MULTIPLY, Blend.DIVIDE)
             update {
-                val loc  = player.location
-                location = loc.clone().apply {
-                    y += (framecount.toDouble() / 4.0 % 4.25) - 1
-                }
-                val size = when {
-                    location.y >=loc.y + 2 -> 1 - (location.y - (loc.y + 2.0))
-                    location.y < loc.y     -> 1 - (loc.y - location.y)
-                    else                   -> 1.0
-                }
-                circle(
-                    vertexes = 15,
-                    size     = size
-                ) { (v) ->
-                    location(v.rotateY(time / 250.0).apply {
-                        if (player.isGliding) {
-                            rotateX((90.0 + location.pitch).radians, center = player.eyeLocation)
-                            rotateY(-location.yaw.toDouble().radians)
-                        }
-                    })
-                    color(colors[(framecount / 17) % colors.size])
+                var g = 0
+                line(
+                    point2   = point.add(Vector(10.0, 0.0, 10.0)),
+                    vertexes = gradients.size
+                ) {
+                    color(gradients[g++].blend(Color.ORANGE, mode = blends[framecount / 60 % blends.size]))
                 }
             }
-            cancel { !player.isOnline }
         }
     }
 }
