440585a57dac2e3019a76400953eaa91ff0ee23a
Atrius <atriusedm@gmail.com>
2020-10-17 05:26:55 -0400

    Sky Dolphins
    Sky Dolphins

--- src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
+++ src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
@@ -1,13 +1,17 @@
 package xyz.atrius.shadercube
 
+import org.bukkit.Particle
+import org.bukkit.entity.Dolphin
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
-import xyz.atrius.shadercube.shader.hologram
+import xyz.atrius.shadercube.shader.entity
 import xyz.atrius.shadercube.shader.shader
+import xyz.atrius.shadercube.shape.Circle
+import xyz.atrius.shadercube.util.vec
 
 typealias KotlinPlugin =
     JavaPlugin
@@ -26,9 +30,25 @@ class ShaderCube : KotlinPlugin(), Listener {
     @EventHandler
     fun onJoin(event: PlayerJoinEvent) {
         val player = event.player
-        shader {
-            location = player.location
-            hologram(point, "Hello World!", "MEME", "TEST", removeAfter = 150)
+        // SKY DOLPHINS
+        repeat(10) {
+            shader {
+                location = player.location.add(0.0, 30.0, 0.0)
+                val dolphin = entity<Dolphin>(point)  {
+                    removeWhenFarAway = false
+                    maximumAir = Int.MAX_VALUE
+                    remainingAir = maximumAir
+                } ?: return@shader
+                update {
+                    dolphin.velocity = dolphin.location.direction.divide(10.vec)
+                    every(20) {
+                        Circle(dolphin.location, Particle.END_ROD, size = 2.5, vertexes = 50) {
+                            extra(0.0)
+                        }
+                    }
+                }
+                cancel { !dolphin.isValid }
+            }
         }
     }
 }
