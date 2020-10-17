a7635b3765f3eca154335c6c5767c9a911d9b17f
Atrius <atriusedm@gmail.com>
2020-10-17 03:53:50 -0400

    Implement despawn timer support for entities

--- src/main/kotlin/xyz/atrius/shadercube/shader/Entity.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shader/Entity.kt
@@ -6,17 +6,25 @@ import org.bukkit.Material
 import org.bukkit.entity.Entity
 import org.bukkit.entity.FallingBlock
 import org.bukkit.util.Vector
+import xyz.atrius.shadercube.util.plugin
+import xyz.atrius.shadercube.util.schedule
 
 inline fun <reified T : Entity> Shader.entity(
     point         : Vector,
+    removeAfter   : Long         = -1,
     noinline block: T.() -> Unit = {}
-) = entity(point.toLocation(world), block)
+) = entity(point.toLocation(world), removeAfter, block)
 
 inline fun <reified T : Entity> Shader.entity(
-    location         : Location  = this.location,
+    location      : Location     = this.location,
+    removeAfter   : Long         = -1,
     noinline block: T.() -> Unit = {}
 ) = try {
-    world.spawn(location, T::class.java, block)
+    world.spawn(location, T::class.java, block).also {
+        if (removeAfter != -1L) schedule.scheduleSyncDelayedTask(
+            plugin, { it.remove() }, removeAfter
+        )
+    }
 } catch (e: Exception) {
     Bukkit.getLogger().warning(e.message)
     null
