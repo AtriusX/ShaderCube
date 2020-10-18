6d22caa804bd5f0aeabd9c87a224eff877626047
Atrius <atriusedm@gmail.com>
2020-10-18 16:30:15 -0400

    Update circle implementation

--- src/main/kotlin/xyz/atrius/shadercube/shape/Circle.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shape/Circle.kt
@@ -3,35 +3,36 @@ package xyz.atrius.shadercube.shape
 import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.util.Vector
+import xyz.atrius.shadercube.data.Coordinate
+import xyz.atrius.shadercube.data.Style
 import xyz.atrius.shadercube.shader.Shader
 import xyz.atrius.shadercube.util.radians
 import xyz.atrius.shadercube.util.vec2d
 
 class Circle(
     override var location: Location,
-    override var particle: Particle      = Particle.REDSTONE,
-                 size    : Double        = 1.0,
-                 vertexes: Int           = 32,
-    override val block   : Style<Circle> = {}
+    override var particle: Particle         = Particle.REDSTONE,
+    private  val radius  : Double           = 1.0,
+    private  val vertexes: Int              = 32,
+    override val style   : Style<Circle> = {}
 ) : Shape<Circle> {
-    override val size  : Vector = size.vec2d
-    override val points: Array<Vector> = Array(vertexes) { point }
+    override val size: Vector = radius.vec2d
+    override val instance: Circle = this
 
-    init {
+    override fun vertexes(): Array<Coordinate> {
+        val vertices = mutableListOf<Coordinate>()
         val angle = (360.0 / vertexes).radians
-        points.forEachIndexed { i, point ->
-            point.rotateY(angle * i, size)
-            particle(particle, point) {
-                block(Data(point, this@Circle))
-            }
+        repeat(vertexes) {
+            vertices += Coordinate(point, point.rotateY(angle * it, radius))
         }
+        return vertices.toTypedArray()
     }
 }
 
 fun Shader.circle(
-    point   : Vector        = this.point,
-    particle: Particle      = Particle.REDSTONE,
-    size    : Double        = 1.0,
-    vertexes: Int           = 32,
-    block   : Style<Circle> = {}
-) = Circle(point.toLocation(world), particle, size, vertexes, block)
+        point   : Vector        = this.point,
+        particle: Particle      = Particle.REDSTONE,
+        radius  : Double        = 1.0,
+        vertexes: Int           = 32,
+        block   : Style<Circle> = {}
+) = Circle(point.toLocation(world), particle, radius, vertexes, block)
--- src/main/kotlin/xyz/atrius/shadercube/shape/Cube.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shape/Cube.kt
@@ -1,45 +1,45 @@
 package xyz.atrius.shadercube.shape
-
-import org.bukkit.Location
-import org.bukkit.Particle
-import org.bukkit.util.Vector
-import xyz.atrius.shadercube.shader.Shader
-import xyz.atrius.shadercube.util.component1
-import xyz.atrius.shadercube.util.component2
-import xyz.atrius.shadercube.util.vec
-
-class Cube(
-    override var location: Location,
-    override var particle: Particle    = Particle.REDSTONE,
-    override val size    : Vector      = 1.vec,
-                 step    : Int         = 5,
-                 hollow  : Boolean     = true,
-                 center  : Boolean     = true,
-    override val block   : Style<Square> = {}
-) : Shape<Square> {
-    override val points: Array<Vector> = arrayOf()
-
-    init {
-        val (_, h) = size
-        val hSize = h * step
-        val p = location.clone()
-        if (center) p.subtract(0.0, h / 2.0, 0.0)
-        for (y in 0..hSize) Square(
-            p, particle, size, step, if (y in listOf(0, hSize)) hollow else true, center, y !in listOf(0, hSize) && hollow
-        ) { (v, s) ->
-            block(Data(v, s))
-        }.also {
-            p.add(0.0, 1.0 / step, 0.0)
-        }
-    }
-}
-
-fun Shader.cube(
-    point   : Vector        = this.point,
-    particle: Particle      = Particle.REDSTONE,
-    size    : Vector        = 1.vec,
-    step    : Int           = 5,
-    hollow  : Boolean       = true,
-    center  : Boolean       = true,
-    block   : Style<Square> = {}
-) = Cube(point.toLocation(world), particle, size, step, hollow, center, block)
+//
+//import org.bukkit.Location
+//import org.bukkit.Particle
+//import org.bukkit.util.Vector
+//import xyz.atrius.shadercube.shader.Shader
+//import xyz.atrius.shadercube.util.component1
+//import xyz.atrius.shadercube.util.component2
+//import xyz.atrius.shadercube.util.vec
+//
+//class Cube(
+//    override var location: Location,
+//    override var particle: Particle    = Particle.REDSTONE,
+//    override val size    : Vector      = 1.vec,
+//                 step    : Int         = 5,
+//                 hollow  : Boolean     = true,
+//                 center  : Boolean     = true,
+//    override val style   : Style<Square> = {}
+//) : Shape<Square> {
+//    override val vertices: Array<Vector> = arrayOf()
+//
+//    init {
+//        val (_, h) = size
+//        val hSize = h * step
+//        val p = location.clone()
+//        if (center) p.subtract(0.0, h / 2.0, 0.0)
+//        for (y in 0..hSize) Square(
+//            p, particle, size, step, if (y in listOf(0, hSize)) hollow else true, center, y !in listOf(0, hSize) && hollow
+//        ) { (v, s) ->
+//            block(Data(v, s))
+//        }.also {
+//            p.add(0.0, 1.0 / step, 0.0)
+//        }
+//    }
+//}
+//
+//fun Shader.cube(
+//    point   : Vector        = this.point,
+//    particle: Particle      = Particle.REDSTONE,
+//    size    : Vector        = 1.vec,
+//    step    : Int           = 5,
+//    hollow  : Boolean       = true,
+//    center  : Boolean       = true,
+//    block   : Style<Square> = {}
+//) = Cube(point.toLocation(world), particle, size, step, hollow, center, block)
