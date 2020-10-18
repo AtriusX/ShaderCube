a06a2187d86ff221ca5d924bc94c7e9ae37e0066
Atrius <atriusedm@gmail.com>
2020-10-18 02:34:30 -0400

    Rainbow spinny text

--- src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
+++ src/main/kotlin/xyz/atrius/shadercube/ShaderCube.kt
@@ -9,6 +9,7 @@ import org.koin.dsl.module
 import xyz.atrius.shadercube.shader.Text
 import xyz.atrius.shadercube.shader.shader
 import xyz.atrius.shadercube.util.hsb
+import xyz.atrius.shadercube.util.vec
 
 typealias KotlinPlugin =
     JavaPlugin
@@ -27,10 +28,13 @@ class ShaderCube : KotlinPlugin(), Listener {
     @EventHandler
     fun onJoin(event: PlayerJoinEvent) {
         val player = event.player
-        shader {
+        shader(3) {
             location = player.location
             update {
-                Text(location, "Hello World!", hsb(framecount / 400f, 1f, 1f))
+                Text(location, "Hello World!", size = 0.75.vec) { (v) ->
+                    location(v.rotateY(time / 8000.0))
+                    color(hsb(framecount / 400f, 1f, 1f))
+                }
             }
             cancel { !player.isOnline }
         }
