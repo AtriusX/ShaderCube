10a3ec49abef1d349655c53b692805d725051a0c
Atrius <atriusedm@gmail.com>
2020-10-18 02:35:04 -0400

    Properly impliment particle text

--- src/main/kotlin/xyz/atrius/shadercube/shader/Text.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shader/Text.kt
@@ -1,40 +1,75 @@
 package xyz.atrius.shadercube.shader
 
-import org.bukkit.Color
 import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.map.MapFont
 import org.bukkit.map.MinecraftFont
+import org.bukkit.util.Vector
+import xyz.atrius.shadercube.shape.Data
+import xyz.atrius.shadercube.shape.Shape
+import xyz.atrius.shadercube.shape.Style
+import xyz.atrius.shadercube.util.indices
+import xyz.atrius.shadercube.util.iterator
+import xyz.atrius.shadercube.util.vec
+
+typealias Sprite =
+    MapFont.CharacterSprite
 
 class Text(
-    val location: Location,
-    message : String,
-    private val color: Color
-) {
-    val data = MinecraftFont.Font
-    val point = location.toVector()
+    override var location: Location,
+                 text    : String,
+                 align   : Align       = Align.CENTER,
+    override val size    : Vector      = 1.vec,
+    override var particle: Particle    = Particle.REDSTONE,
+    override val block   : Style<Text> = {}
+): Shape<Text> {
+    override val points: Array<Vector> = arrayOf()
+
+    private val data    = MinecraftFont.Font
+    private val sprites = if (data.isValid(text)) text
+        .mapNotNull { data.getChar(it) }
+        .map { it to it.width / (4.0 / size.x) }.toList()
+    else throw IllegalStateException("Illegal characters in string: $text")
+
+    private var baseOffset = if (align != Align.LEFT) {
+        val base = sprites.sumByDouble { it.second }
+        if (align == Align.CENTER) base / 2 else base
+    } else 0.0
 
     init {
         var offset = 0.0
-        val sprites = if (data.isValid(message))
-            message.mapNotNull { data.getChar(it) } else null
-        sprites?.forEachIndexed { _, s ->
-            render(s, offset)
-            offset += s.width / 4.0
+        sprites.forEach { (s, w) ->
+            render(s, offset - baseOffset)
+            offset += w
         }
     }
 
-    private fun render(sprite: MapFont.CharacterSprite, offset: Double) {
-        for (y in 0 until sprite.height)
-            for (x in 0 until sprite.width) {
-                if (sprite[y, x]) location.world.spawnParticle(
-                    Particle.REDSTONE, offset + location.x + x / 5f, location.y - y / 5f, 0.0, 1,
-                        Particle.DustOptions(color, 1f)
-                )
+    private fun render(sprite: Sprite, offset: Double) {
+        for ((y, x) in
+            sprite.height.indices to sprite.width.indices
+        ) if (sprite[y, x]) {
+            val p = Vector(
+                offset + point.x + x / (5f / size.x),
+                         point.y - y / (5f / size.z),
+                         point.z
+            )
+            particle(particle, p) {
+                block(Data(p, this@Text))
             }
+        }
     }
 }
 
-fun Shader.text(text: String) {
+fun Shader.text(
+    point   : Vector      = this.point,
+    text    : String,
+    align   : Align       = Align.CENTER,
+    size    : Vector      = 1.vec,
+    particle: Particle    = Particle.REDSTONE,
+    block   : Style<Text> = {}
+) = Text(point.toLocation(world), text, align, size, particle, block)
 
+@Suppress("unused")
+enum class Align {
+    LEFT, CENTER, RIGHT
 }
--- src/main/kotlin/xyz/atrius/shadercube/util/Misc.kt
+++ src/main/kotlin/xyz/atrius/shadercube/util/Misc.kt
@@ -30,3 +30,14 @@ fun Color.compliments(count: Int = 1): Array<Color> = toHSB().run {
 
 fun Color.plusCompliments(count: Int = 1): Array<Color> =
     arrayOf(this, *compliments(count))
+
+val Int.indices: IntRange
+    get() = 0 until this
+
+operator fun Pair<IntRange, IntRange>.iterator(): Iterator<Pair<Int, Int>> {
+    val (first, second) = this
+    val data = mutableListOf<Pair<Int, Int>>()
+    for (i in first) for (j in second)
+        data += i to j
+    return data.iterator()
+}
