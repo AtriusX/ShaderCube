d7fdc9dfed85082412d5c1652f05d114d0886ba1
Atrius <atriusedm@gmail.com>
2020-10-19 00:16:42 -0400

    Fix rotation breaking with coordinate system

--- src/main/kotlin/xyz/atrius/shadercube/data/Coordinate.kt
+++ src/main/kotlin/xyz/atrius/shadercube/data/Coordinate.kt
@@ -11,27 +11,30 @@ class Coordinate(
 
     constructor(center: Vector, relative: Vector) : this(center, relative.x, relative.y, relative.z)
 
+    val relative: Vector = Vector(x - center.x, y - center.y, z - center.z)
+
     val relX: Double
-        get() = x - center.x
+        get() = relative.x
 
     val relY: Double
-        get() = y - center.y
+        get() = relative.y
 
     val relZ: Double
-        get() = z - center.z
+        get() = relative.z
 
     override fun rotateAroundX(angle: Double): Vector = subtract(center).also {
         super.rotateAroundX(angle).add(center)
     }
 
-    override fun rotateAroundY(angle: Double): Vector = subtract(center).also {
-        super.rotateAroundY(angle).add(center)
+    override fun rotateAroundY(angle: Double): Vector {
+        relative.rotateAroundY(angle)
+        return get()
     }
 
     override fun rotateAroundZ(angle: Double): Vector = subtract(center).also {
         super.rotateAroundZ(angle).add(center)
     }
 
-    fun relative(): Vector =
-        Vector(relX, relY, relZ)
+    fun get(): Vector =
+        center.clone().add(relative)
 }
