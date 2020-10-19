b4624ac6290b9fe2b924ab139750b9f190cd058a
Atrius <atriusedm@gmail.com>
2020-10-18 22:03:37 -0400

    Reimplement Square primitive

--- src/main/kotlin/xyz/atrius/shadercube/shape/Square.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shape/Square.kt
@@ -3,39 +3,46 @@ package xyz.atrius.shadercube.shape
 import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.util.Vector
+import xyz.atrius.shadercube.data.Coordinate
+import xyz.atrius.shadercube.data.Style
 import xyz.atrius.shadercube.shader.Shader
+import xyz.atrius.shadercube.util.iterator
 import xyz.atrius.shadercube.util.vec2d
 
 class Square(
     override var location: Location,
     override var particle: Particle      = Particle.REDSTONE,
     override val size    : Vector        = 1.vec2d,
-                 step    : Int           = 5,
-                 hollow  : Boolean       = true,
-                 center  : Boolean       = true,
-                 corners : Boolean       = false,
-    override val block   : Style<Square> = {}
+    private  val step    : Int           = 5,
+    private  val hollow  : Boolean       = true,
+    private  val centered: Boolean       = true,
+    private  val corners : Boolean       = false,
+    override val style   : Style<Square> = {}
 ) : Shape<Square> {
-    override val points: Array<Vector> = Array(size.blockX * size.blockZ) { point }
 
-    init {
-        val w = size.x
-        val h = size.z
-        val wSize = (w * step).toInt()
-        val hSize = (h * step).toInt()
-        for (x in 0..wSize step if (corners) wSize else 1)
-            for (z in 0..hSize step if (corners) hSize else 1)
-                if (!hollow || (x in listOf(0, wSize) || z in listOf(0, hSize))) {
-                    val pos = Vector(
-                        point.x + x.toDouble() / step - if (center) w / 2 else 0.0,
-                        point.y,
-                        point.z + z.toDouble() / step - if (center) h / 2 else 0.0
-                    )
-                    particle(particle, pos) {
-                        block(Data(pos, this@Square))
-                    }
-                }
+    private val w     = size.x
+    private val h     = size.z
+    private val wSize = (w * step).toInt()
+    private val hSize = (h * step).toInt()
+
+    override fun vertexes(): Array<Coordinate> {
+        val vertices = mutableListOf<Coordinate>()
+        for((x, z) in range(wSize) to range(hSize)) if (!hollow || isEdge(x, z)) {
+            vertices += Coordinate(point, Vector(
+                adjust(point.x, x.toDouble(), w), point.y, adjust(point.z, z.toDouble(), h)
+            ))
+        }
+        return vertices.toTypedArray()
     }
+
+    private fun range(size: Int): IntProgression =
+        0..size step if (corners) size else 1
+
+    private fun isEdge(x: Int, z: Int): Boolean =
+        x in listOf(0, wSize) || z in listOf(0, hSize)
+
+    private fun adjust(value: Double, other: Double, size: Double): Double =
+        value + other / step - if (centered) size / 2 else 0.0
 }
 
 fun Shader.square(
