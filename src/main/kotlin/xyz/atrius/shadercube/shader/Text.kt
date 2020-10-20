4ff2ba7a6bd0527b8a31c267de26008dfdc9fa2b
Atrius <atriusedm@gmail.com>
2020-10-20 15:42:42 -0400

    Document Text object

--- src/main/kotlin/xyz/atrius/shadercube/shader/Text.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shader/Text.kt
@@ -1,3 +1,4 @@
+@file:Suppress("unused")
 package xyz.atrius.shadercube.shader
 
 import org.bukkit.Location
@@ -12,9 +13,30 @@ import xyz.atrius.shadercube.util.indices
 import xyz.atrius.shadercube.util.iterator
 import xyz.atrius.shadercube.util.vec
 
+/**
+ * @suppress Maps the Minecraft font's character data to a simplified type.
+ */
 typealias Sprite =
     MapFont.CharacterSprite
 
+/**
+ * This is a complex shape type that generates coordinates based off the internally
+ * provided Minecraft font. This in effect allows us to create particle systems that
+ * match the look of text in-game.
+ *
+ * @constructor       Constructs a physical text object made up of particles.
+ * @property location The center point that this text object is mapped to.
+ * @param    text     The text to in the coordinate array.
+ * @param    align    The alignment used for this text. Can be either [Align.LEFT],
+ *                    [Align.RIGHT] or [Align.CENTER]. Alignment also determines
+ *                    the origin point for text is located at.
+ * @property size     The size used for the text scale. X controls width while Z
+ *                    controls height. Y is ignored.
+ * @property particle The particle for which this object will use during rendering cycles.
+ * @property style    The style applied to this object.
+ *
+ * @see Style
+ */
 class Text(
     override var location: Location,
                  text    : String,
@@ -64,6 +86,23 @@ class Text(
     }
 }
 
+/**
+ * This is a simple method to create text systems for use in shader scripts. An included
+ * benefit of this method is the inclusion of multi-line text support. For a full rundown
+ * on this object, please view [Text].
+ *
+ * @property point    The center point that this text object is mapped to.
+ * @param    text     The text to in the coordinate array.
+ * @param    align    The alignment used for this text. Can be either [Align.LEFT],
+ *                    [Align.RIGHT] or [Align.CENTER]. Alignment also determines
+ *                    the origin point for text is located at.
+ * @property size     The size used for the text scale. X controls width while Z
+ *                    controls height. Y is ignored.
+ * @property particle The particle for which this object will use during rendering cycles.
+ * @property block    The style applied to this object.
+ *
+ * @see Text
+ */
 fun Shader.text(
     point      : Vector      = this.point,
     text       : String,
@@ -76,7 +115,28 @@ fun Shader.text(
     Text(point.toLocation(world).subtract(0.0, (size.z * lineSpacing) * i, 0.0), it, align, size, particle, block)
 }.toTypedArray()
 
-@Suppress("unused")
+/**
+ * This object is used for determining the alignment of a text object.
+ * Setting this will realign the text margin and origin points for the
+ * given [Text] object.
+ */
 enum class Align {
-    LEFT, CENTER, RIGHT
+
+    /**
+     * @property LEFT Aligns the text margin to the left and sets the
+     *                origin point to the left side.
+     */
+    LEFT,
+
+    /**
+     * @property CENTER Aligns the text margin to the center and sets the
+     *                  origin point to the center.
+     */
+    CENTER,
+
+    /**
+     * @property RIGHT Aligns the text margin to the right and sets the
+     *                 origin point to the right side.
+     */
+    RIGHT
 }
