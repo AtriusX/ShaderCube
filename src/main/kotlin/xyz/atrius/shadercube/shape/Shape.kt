797f99ff8adcfa77e40b734da88a051651d446ae
Atrius <atriusedm@gmail.com>
2020-10-21 05:24:53 -0400

    Add shape documentation

--- src/main/kotlin/xyz/atrius/shadercube/shape/Shape.kt
+++ src/main/kotlin/xyz/atrius/shadercube/shape/Shape.kt
@@ -1,25 +1,51 @@
 package xyz.atrius.shadercube.shape
 
-import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.util.Vector
-import xyz.atrius.shadercube.data.Spatial
-import xyz.atrius.shadercube.data.Coordinate
-import xyz.atrius.shadercube.data.Data
-import xyz.atrius.shadercube.data.Stylable
-import xyz.atrius.shadercube.data.Updatable
+import xyz.atrius.shadercube.data.*
 
+/**
+ * Shapes are a basic object that can be used in the creation of more complex
+ * shaders or animations. Since the purpose of shapes is to generate physical
+ * geometry rather than to render particles, this class implements a generified
+ * [update] function. Implementations should **not** attempt to override this
+ * function and should instead focus their efforts on generating the shape's
+ * vertex list.
+ *
+ * @see update
+ */
 abstract class Shape<T> : Spatial, Updatable, Stylable<T> {
 
+    /**
+     * @property particle The particle that is used when [update] is called.
+     */
     abstract var particle: Particle
 
-    val center: Location
-        get() = location
-
+    /**
+     * @property size The dimensions of any shape instances.
+     */
     abstract val size: Vector
 
+    /**
+     * @property vertices The vertex list which stores all the points used
+     *                    for generating particles when [update] is called.
+     */
     var vertices: MutableList<Coordinate> = mutableListOf()
 
+    /**
+     * This function is implemented by extensions of this class and is
+     * used for generating a list of vertexes that should be stored in
+     * the [vertices] vertex list.
+     */
+    abstract fun vertexes()
+
+    /**
+     * Rotates a shape instance on all main axis relative to the center point.
+     *
+     * @param x The X rotation angle.
+     * @param y The Y rotation angle.
+     * @param z The Z rotation angle.
+     */
     fun rotate(x: Double, y: Double, z: Double) = vertices.forEach {
         it.rotateX(x).rotateY(y).rotateZ(z)
     }
@@ -30,6 +56,4 @@ abstract class Shape<T> : Spatial, Updatable, Stylable<T> {
             style(Data(Coordinate(point, it), this@Shape as T))
         }
     }
-
-    abstract fun vertexes()
 }
